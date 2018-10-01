3004f33f436476c5f7ffc05aaca132d38134052d
takewiki <hulilei@takewiki.com.cn>
2018-10-02 06:48:30 +0800

    add the file type

--- R/zipFile.R
+++ R/zipFile.R
@@ -3,3 +3,8 @@ zip("./zipfile/aaa.zip",c("./zipfile/8月DC.xlsx","./zipfile/8月DC_处理后.xl
 
 #使用新的目录
 zip("./download",c("./zipfile/8月DC.xlsx","./zipfile/8月DC_处理后.xlsx"));
+
+library(shiny)
+downloadHandler(filename = 'abc.zip',content = function(file){},
+                contentType = 'application/zip')
+#这是需要处理的地方;
